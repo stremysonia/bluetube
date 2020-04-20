fc39040117c1617806cd0bcaeacc4e42d91afb16
Jaboa <jaboahowell@gmail.com>
2020-04-20 12:15:34 -0600

    made category boxes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { Grid } from "@material-ui/core";
+
 import SearchBar from "./components/SearchBar";
 import VideoDetail from "./components/VideoDetail";
 import VideoList from "./components/VideoList";
 import youtube from "./api/youtube";
+import Category from "./components/Category";
 import "./App.css";
 
 function App() {
@@ -22,25 +23,24 @@ function App() {
     setVideo(resp.data.items);
     setSelectedVideo(resp.data.items[0]);
 
-    console.log(resp);
+    console.log(video);
   };
 
   return (
-    <Grid justify="center" container spacing={10}>
-      <Grid item xs={8}>
-        <Grid container spacing={10}>
-          <Grid item xs={8}>
-            <SearchBar onFormSubmit={handleSubmit} />
-          </Grid>
-          <Grid item xs={6}>
-            <VideoDetail video={selectedVideo} />
-          </Grid>
-          <Grid item xs={4}>
-            <VideoList />
-          </Grid>
-        </Grid>
-      </Grid>
-    </Grid>
+    <div>
+      <div>
+        <SearchBar onFormSubmit={handleSubmit} />
+      </div>
+      <div>
+        <Category />
+      </div>
+      <div>
+        <VideoDetail video={selectedVideo} />
+      </div>
+      <div>
+        <VideoList video={video} />
+      </div>
+    </div>
   );
 }
 
